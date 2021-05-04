8a03c0f2f4ba77f7c435505cda55279b7afbbf7e
Enes Erdogan <eneserdo98@gmail.com>
2021-05-04 19:17:57 +0300

    Day 3

--- Training.py
+++ Training.py
@@ -9,65 +9,64 @@ import numpy as np
 
 
 def main(opt):
-
     print(opt)
-    exit()
+
     # Parameters
     lambda_D = 5.0
     lambda_FM = 1
     lambda_P = 0.5
 
     epoch = 1000
-    batch_size=10
+    batch_size = 10
 
-    # Preparation for Training
-
+    ####### Preparation for Training #######
+    # Load the networks
     assert t.cuda.is_available()
     device = "cuda"
 
-    disc=networks.MultiScaleDisc()
-    gen=networks.Generator()
+    disc = networks.MultiScaleDisc()
+    gen = networks.Generator()
 
-    if opt.current_epoch!=0:
-        loss_change_g=np.load("checkpoints/loss_change_g.npy")
-        loss_change_d=np.load("checkpoints/loss_change_d.npy")
+    if opt.current_epoch != 0:
+        loss_change_g = np.load("checkpoints/loss_change_g.npy")
+        loss_change_d = np.load("checkpoints/loss_change_d.npy")
         # TODO load models
     else:
-        loss_change_g=np.array([])
-        loss_change_d=np.array([])
+        loss_change_g = np.array([])
+        loss_change_d = np.array([])
         utils.weights_init(disc.disc1)
         utils.weights_init(disc.disc2)
         utils.weights_init(gen)
 
-    optim_g=optim.Adam(gen.parameters(), lr=0.0002, betas=(0.5, 0.999))
-    optim_d=optim.Adam(disc.parameters(), lr=0.0002, betas=(0.5, 0.999))
+    # Create optimizers
+    optim_g = optim.Adam(gen.parameters(), lr=0.0002, betas=(0.5, 0.999))
+    optim_d = optim.Adam(disc.parameters(), lr=0.0002, betas=(0.5, 0.999))
 
+    # Create loss functions
+    loss = losses.GanLoss()
+    loss_fm = losses.FeatureMatchingLoss()
+    loss_p = losses.VGGLoss()
 
-    loss=losses.GanLoss()
-    loss_fm=losses.FeatureMatchingLoss()
-    loss_p=losses.VGGLoss()
-
-    DS=dataset.Dataset(opt.data_dir)
-
-    dataloader = DataLoader(DS, batch_size=batch_size, shuffle=True, num_workers=2)
+    # Create dataloader
+    ds = dataset.CustomDataset(opt.data_dir)
+    dataloader = DataLoader(ds, batch_size=batch_size, shuffle=True, num_workers=2)
 
     # Start to training
-
     for e in range(epoch):
         for ir, rgb, segment in tqdm(dataloader):
-
-            condition=t.cat([rgb, segment], dim=1)
+            condition = t.cat([rgb, segment], dim=1)
 
             ir_pred = gen(condition)
 
             # Updating Generator
             optim_g.zero_grad()
 
-            out1_pred, out2_pred=disc(ir_pred)
-            out1, out2=disc(ir)
+            out1_pred, out2_pred = disc(ir_pred)
+            out1, out2 = disc(ir)
 
-            gen_loss=loss(out1_pred[-1], out2_pred[-1], is_real=True)*lambda_D+(loss_fm(out1_pred[:-1], out1[:-1])+loss_fm(out2_pred[:-1], out2[:-1]))*lambda_FM+ \
-                +lambda_P*loss_p(ir_pred, ir)
+            gen_loss = loss(out1_pred[-1], out2_pred[-1], is_real=True) * lambda_D + (
+                        loss_fm(out1_pred[:-1], out1[:-1]) + loss_fm(out2_pred[:-1], out2[:-1])) * lambda_FM + \
+                       +lambda_P * loss_p(ir_pred, ir)
 
             # loss_change_g
             gen_loss.backward()
@@ -77,15 +76,15 @@ def main(opt):
             # Updating Discriminator.
             optim_d.zero_grad()
 
-            disc_loss=loss(out1_pred[-1].detach(), out2_pred[-1].detach(), is_real=False)+loss(out1[-1], out2[-1], is_real=True)
+            disc_loss = loss(out1_pred[-1].detach(), out2_pred[-1].detach(), is_real=False) + loss(out1[-1], out2[-1],
+                                                                                                   is_real=True)
 
             disc_loss.backward()
 
             optim_d.step()
 
-
-
         # TODO save the model and image
+
 
 
 if __name__ == '__main__':
--- utils/dataset.py
+++ utils/dataset.py
@@ -1,25 +1,57 @@
 import torch as t
-# from torch.utils.data import DataLoader
+from skimage import io
 import os
+from torchvision import transforms
+from torch.utils.data import Dataset
 
-class Dataset(t.utils.data.Dataset):
+
+# import fnmatch
+# print len(fnmatch.filter(os.listdir(dirpath), '*.txt'))
+
+class CustomDataset(Dataset):
 
     def __init__(self, root_dir):
 
-        rgb_dir=os.path.join(root_dir, 'rgb')
-        ir_dir=os.path.join(root_dir, 'ir')
-        segment_dir=os.path.join(root_dir, 'segment')
+        self.rgb_dir = os.path.join(root_dir, 'rgb')
+        self.ir_dir = os.path.join(root_dir, 'ir')
+        self.segment_dir = os.path.join(root_dir, 'segment')
 
-        self.labels = None
-        self.data=None
+        # Check dir exist
+
+        assert os.path.isdir(self.rgb_dir), 'Error: No rgb dir'
+        assert os.path.isdir(self.ir_dir), 'Error: No ir dir'
+        assert os.path.isdir(self.segment_dir), 'Error: No segment dir'
+
+        self.composed = transforms.Compose([transforms.ToTensor(),
+                                            transforms.Normalize((0.34, 0.33, 0.35), (0.19, 0.18, 0.18))])
+
+        _, _, files1 = next(os.walk(self.rgb_dir))
+        _, _, files2 = next(os.walk(self.ir_dir))
+        _, _, files3 = next(os.walk(self.segment_dir))
+
+        assert len(files1) == len(files2) and len(files3) == len(
+            files2), f"files are different rgb:{len(files1)}, ir:{len(files2)} segment:{len(files3)}"
+
+        self.L = len(files1)
+
+        print(f"Custom dataset initialized with {self.L} images")
 
     def __len__(self):
-
-        return len(self.labels)
+        return self.L
 
     def __getitem__(self, index):
 
-        data_t=t.tensor(self.data[index], dtype=t.float)
-        label_t=t.tensor(self.labels[index], dtype=t.float)
+        segment = (io.imread(os.path.join(self.segment_dir, f'{index}.jpg'))) / 255.0
+        segment = segment.transpose(2, 0, 1)
+        segment = self.composed(segment)
 
-        return data_t, label_t
+        rgb = (io.imread(os.path.join(self.rgb_dir, f'{index}.jpg'))) / 255.0
+        rgb = rgb.transpose(2, 0, 1)
+        rgb = self.composed(rgb)
+
+        ir = (io.imread(os.path.join(self.ir_dir, f'{index}.jpg'))) / 255.0
+        ir = ir.transpose(2, 0, 1)
+        ir = self.composed(ir)
+
+        return rgb, segment, ir
+
